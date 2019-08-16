e1536febf6d8a43bc7a9e4b498a61aeb4dda20f3
Raman Hrytsuk <Raman_Hrytsuk@epam.com>
2019-08-16 15:42:15 +0300

    problem

--- VTS_EPAM/Activities/SplashActivity.cs
+++ VTS_EPAM/Activities/SplashActivity.cs
@@ -1,26 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
 
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using Android.Support.V7.App;
+using Android.Util;
+using System.Threading.Tasks;
 
 namespace VTS_EPAM.Activities
 {
-    [Activity(Label = "SplashActivity", MainLauncher=true, Theme = "@style/Theme.Splash", NoHistory = true)]
-    public class SplashActivity : Activity
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
 
-            StartActivity(typeof(SplashActivity));
-            // Create your application here
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+            
+            Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
     }
 }
