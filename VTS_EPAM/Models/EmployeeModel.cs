8bd6846d1a25f7dc5967f018899c6ef887a2f780
Raman Hrytsuk <Raman_Hrytsuk@epam.com>
2019-08-19 13:31:03 +0300

    add models

---
+++ VTS_EPAM/Models/EmployeeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace VTS_EPAM.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public List<VacationModel> Vacations { get; }
+
+        public EmployeeModel()
+        {
+            Vacations = new List<VacationModel>();
+        }
+    }
+}
---
+++ VTS_EPAM/Models/VacationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace VTS_EPAM.Models
+{
+    public class VacationModel
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public string DateFrom { get; set; }
+        public string DateTo { get; set; }
+    }
+}
